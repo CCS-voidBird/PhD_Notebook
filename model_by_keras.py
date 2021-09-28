17a9c0d8694fc596e6642af90eb8cc81e006b58b
ChensongC <706512884@qq.com>
2021-09-28 14:30:56 +1000

    Update model_by_keras.py

--- model_by_keras.py
+++ model_by_keras.py
@@ -10,12 +10,13 @@ import tensorflow as tf
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import OneHotEncoder
 
 GENO_PATH = "E:\learning resource\PhD\geno_data1.csv"
 PHENO_PATH = "E:\learning resource\PhD\phenotypes.csv"
 
-TRAIN_PATH = "E:/learning resource/PhD/sugarcane/2016_TCHBlup_2000.csv"
-VALID_PATH = "E:/learning resource/PhD/sugarcane/2015_TCHBlup_2000.csv"
+TRAIN_PATH = "E:/learning resource/PhD/sugarcane/2015_TCHBlup_2000.csv"
+VALID_PATH = "E:/learning resource/PhD/sugarcane/2016_TCHBlup_2000.csv"
 LABEL_COLUMN = 'TCHBlup'
 sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=0)
 
@@ -29,17 +30,22 @@ def modelling(n_layers,n_units,input_shape):
     #model.add(Dropout(0.2))
     model.add(Conv1D(32, kernel_size=3, strides=1, padding='valid',activation='elu'))
     model.add(MaxPooling1D(pool_size=2))
-    model.add(Conv1D(16, kernel_size=3, strides=1, padding='valid',activation='relu'))
-    model.add(MaxPooling1D(pool_size=2))
+    #model.add(Conv1D(16, kernel_size=3, strides=1, padding='valid',activation='relu'))
+    #model.add(MaxPooling1D(pool_size=2))
+
     model.add(Flatten())
     for layers in range(n_layers):
         model.add(Dense(n_units,activation="elu"))
     model.add(Dropout(0.2))
     #model.add(Dense(n_layers,activation="linear"))
-    model.add(Dense(n_layers, activation="linear"))
+    #model.add(Dense(n_layers, activation="linear"))
     model.add(Dense(1, activation="linear"))
     tf.keras.optimizers.RMSprop(learning_rate=0.00001)
     model.compile(optimizer="rmsprop",loss="mean_squared_error")
+
+    """
+    Optimizers: Adam, RMSProp, Momentum 
+    """
 
     return model
 
@@ -55,18 +61,36 @@ def plot_loss_history(h, title):
 def main():
 
     #prepare data from csv files
-    train_data = pd.read_csv(TRAIN_PATH,sep="\t").drop(columns="Region")
-    valid_data = pd.read_csv(VALID_PATH,sep="\t").drop(columns="Region") #The final valid data
+    train_data = pd.read_csv(TRAIN_PATH,sep="\t")  #.drop(columns="Region")
+    valid_data = pd.read_csv(VALID_PATH,sep="\t")  #.drop(columns="Region") The final valid data
+
+    """
+    Drop sampling index 
+    """
+    train_data.drop(train_data.columns[0],axis=1, inplace=True)
+    valid_data.drop(valid_data.columns[0], axis=1, inplace=True)
+
+
+    print("Train data:")
+    print(train_data.head(3))
+    print(train_data.iloc[:,0])
+    print("Valid data:")
+    print(valid_data.head(3))
 
     #pro-process data, add dim and select features
-    train_targets = train_data["TCHBlup"].values
+
+    train_targets = train_data["TCHBlup"].values   #Get the target values from train set
     train_features = train_data.iloc[:,2:]
+    #ohe = OneHotEncoder()
+    #ohe.fit(train_features)  # hot encode region data
+    #train_features = ohe.transform(train_features)
 
     n_features = train_features.shape[1]
     train_features = np.expand_dims(train_features,axis=2)
 
     valid_targets = valid_data["TCHBlup"].values
     valid_features = valid_data.iloc[:, 2:]
+    #valid_features = ohe.transform(valid_features) # hot encode region data
     valid_features = np.expand_dims(valid_features, axis=2)
 
     #split train data into 2 part - train and test
@@ -75,7 +99,7 @@ def main():
     input_size = (n_features, 1)
     val_loss = 200
     while val_loss >= 70:
-        model = modelling(n_layers=3, n_units=5, input_shape=input_size)
+        model = modelling(n_layers=3, n_units=8, input_shape=input_size)
         print(model.summary())
         history = model.fit(
             features_train, target_train,
@@ -89,9 +113,9 @@ def main():
         val_loss = history.history['val_loss'][-1]
 
         y_pred = np.reshape(model.predict(valid_features), (2000,))
-        print(y_pred.shape, valid_targets.shape)
-        print(y_pred)
-        print(valid_targets)
+        #print(y_pred.shape, valid_targets.shape)
+        print("Predicted: ",y_pred)
+        print("observed: ",valid_targets)
         accuracy = np.corrcoef(y_pred, valid_targets)
         print("accuracy (measured as Pearson's correlation) is: ", accuracy)
         if history.history['val_loss'][-1] < val_loss:
