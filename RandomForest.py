bf250c7aa95d5799b48fd0c693461c860c793695
ChensongC <706512884@qq.com>
2021-11-02 15:54:53 +1000

    update

--- RandomForest.py
+++ RandomForest.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 from Functions import *
+from sklearn.datasets import make_regression
 
 def rf_train(train_data,valid_data):
 
@@ -10,7 +11,10 @@ def rf_train(train_data,valid_data):
 
 
 def main():
-    pass
+    X, y = make_regression(n_features=4, n_informative=2,random_state = 0, shuffle = False)
+    regr = RandomForestRegressor(max_depth=2, random_state=0)
+    regr.fit(X, y)
+    print(regr.predict([[0, 0, 0, 0]]))
 
 if __name__ == "__main__":
     main()
