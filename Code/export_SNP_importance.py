c19d98ac0f780186f321e9db07056df8d68e11aa
ChensongC <706512884@qq.com>
2022-05-02 22:08:46 +1000

    update

--- Code/export_SNP_importance.py
+++ Code/export_SNP_importance.py
@@ -12,7 +12,7 @@ for trait in traits:
     for region in regions:
         combos = combos + [(trait,region)]
         # Load the model
-        model = joblib.load("E:/learning resource/PhD/HPC_Results/RF_region/new_complete_model/models/"+trait+"_RF_"+region+"_model.json")
+        model = joblib.load("E:/learning resource/PhD/HPC_Results/RF_region/rerun/models/"+trait+"_RF_"+region+"_model.json")
         # Get the feature importances as list with its trait and region
         importances.append([trait,region]+list(model.feature_importances_))
         print(model.feature_importances_.shape)
@@ -21,4 +21,4 @@ for trait in traits:
 importances = pd.DataFrame(importances,columns=["Trait","Region"]+list(range(1,model.feature_importances_.shape[0]+1)))
 print(importances.shape)
 # write to csv by tab
-importances.to_csv("E:/learning resource/PhD/HPC_Results/RF_region/new_complete_model/RF_SNP_importances.csv",sep="\t",index=False)
+importances.to_csv("E:/learning resource/PhD/HPC_Results/RF_region/rerun/RF_SNP_importances.csv",sep="\t",index=False)
